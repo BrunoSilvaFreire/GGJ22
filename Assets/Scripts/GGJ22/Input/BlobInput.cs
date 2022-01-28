f755ca8015fedc09081547e433638c37c6bd5fd2
BrunoSilvaFreire <brunorbsf@gmail.com>
2022-01-28 20:30:19 -0300

    > Novak = Cringe

--- Assets/Scripts/GGJ22/Input/BlobInput.cs
+++ Assets/Scripts/GGJ22/Input/BlobInput.cs
@@ -3,6 +3,6 @@ using Lunari.Tsuki2D.Input.Platformer;
 namespace GGJ22.Input {
     public class BlobInput : PlatformerInput {
         public EntityAction shoot;
-        public bool pulling;
+        public EntityAction pull;
     }
 }
--- Assets/Scripts/GGJ22/Input/BlobInputSource.cs
+++ Assets/Scripts/GGJ22/Input/BlobInputSource.cs
@@ -3,18 +3,25 @@ using UnityEngine.InputSystem;
 namespace GGJ22.Input {
     public class BlobInputSource : InputSystemPlatformerSource {
         private InputAction shoot;
+        private InputAction pull;
         public string shootInputName = "shoot";
+        public string pullInputName = "pull";
         protected override void Start() {
             base.Start();
             shoot = input.actions[shootInputName];
+            pull = input.actions[pullInputName];
         }
         public bool GetShoot() {
+            return shoot.triggered || shoot.inProgress;
+        }
+        public bool GetPull() {
             return shoot.triggered || shoot.inProgress;
         }
         protected override void TransferTo(PlatformerInput input) {
             base.TransferTo(input);
             if (input is BlobInput i) {
                 i.shoot.Current = GetShoot();
+                i.pull.Current = GetPull();
             }
         }
     }
